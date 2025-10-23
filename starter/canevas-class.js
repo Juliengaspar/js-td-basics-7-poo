5081e26631796ef42e8896626b47a1b49b727443
Juliengaspar <julien.gaspar@student.hepl.be>
2025-10-23 17:24:30 +0200

    first ex , my first classe

--- starter/canevas-class.js
+++ starter/canevas-class.js
@@ -3,22 +3,31 @@
 // 1. Création de la classe
 
 class MaClasse {
+	param1;
+	param2;
 	constructor(param1, param2 /* ... */) {
 		// Définis les propriétés de la classe en utilisant les paramètres
-		this.propriete1 = param1;
-		this.propriete2 = param2;
+		this.param1 = param1;
+		this.param2 = param2;
+
 		// ...
 	}
 	methode1(/* ... */) {
 		// Définis les méthodes de la classe
 		// ...
+		console.log('methode1');
+		console.log(this.param1);
 	}
 	methode2(/* ... */) {
 		// ...
+		console.log('methode2');
+		console.log(this.param2);
 	}
 	// ...
 }
 
 // 2. Utilisation de la classe pour instancier un objet
 
-const monObjet = new MaClasse(arg1, arg2, /* ... */); // Instancie un objet de la classe en fournissant les arguments nécessaires
+const monObjet = new MaClasse('test', 'coucocu', /* ... */); // Instancie un objet de la classe en fournissant les arguments nécessaires
+monObjet.methode1();
+monObjet.methode2();
